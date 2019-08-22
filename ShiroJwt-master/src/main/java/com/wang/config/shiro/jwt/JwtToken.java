d49483ddade160a1151610cda1c58d6d775ce58a
zhihuiaifeng <1206195526@qq.com>
2019-08-22 21:45:01 +0800

    JWT

---
+++ ShiroJwt-master/src/main/java/com/wang/config/shiro/jwt/JwtToken.java
@@ -0,0 +1,32 @@
+package com.wang.config.shiro.jwt;
+
+import org.apache.shiro.authc.AuthenticationToken;
+
+/**
+ * JWTToken差不多就是Shiro用户名密码的载体
+ * JwtToken
+ * @author Wang926454
+ * @date 2018/8/30 14:06
+ */
+public class JwtToken implements AuthenticationToken {
+    /**
+     * Token
+     */
+    private String token;
+
+    public JwtToken(String token) {
+        this.token = token;
+    }
+
+    @Override
+    public Object getPrincipal() {
+
+        return token;
+    }
+
+    @Override
+    public Object getCredentials() {
+
+        return token;
+    }
+}
